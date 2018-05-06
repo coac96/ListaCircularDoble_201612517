a36ce9cd674f4ee451f1c03af1aeb49f974cf0aa
coac_96 <coac_96@DESKTOP-0T6CD3O>
2018-05-06 16:20:10 -0600

    Creacion clases Persona y nodo

---
+++ src/circulardoble_201612517/Persona_201612517.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package circulardoble_201612517;
+
+/**
+ *
+ * @author coac_96
+ */
+public class Persona_201612517 {
+    String nombre;
+    int id;
+
+    public Persona_201612517(String nombre, int id) {
+        this.nombre = nombre;
+        this.id = id;
+    }
+
+}
