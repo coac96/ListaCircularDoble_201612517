cbc6efc78fa1338ff60e657993fb588ddd770f00
coac_96 <coac_96@DESKTOP-0T6CD3O>
2018-05-06 20:04:49 -0600

    Creacion metodo eliminar, buscar, mostrar....
    Prueba Eliminar

--- src/circulardoble_201612517/ListaCircularDobleEjemplo_201612517.java
+++ src/circulardoble_201612517/ListaCircularDobleEjemplo_201612517.java
@@ -10,6 +10,7 @@ package circulardoble_201612517;
  * @author coac_96
  */
 public class ListaCircularDobleEjemplo_201612517 {
+    int tamaño;
     Nodo primero;
     Nodo ultimo;
 
@@ -18,22 +19,83 @@ public class ListaCircularDobleEjemplo_201612517 {
         ultimo = null;
     }
     
-    public void insertar(Object x){
+    public void insertar(Persona_201612517 x){
         Nodo nuevo = new Nodo();
+        Nodo aux = new Nodo();
+        aux = primero;
+        Boolean verificador = false;
         nuevo.objeto = x;
         if(primero == null){
             primero = nuevo;
             primero.siguiente = primero;
-            nuevo.atras = ultimo; 
+            nuevo.atras = ultimo;
             ultimo = nuevo;
         }else{
+            do{
+                if(x.id == aux.objeto.id){
+                    System.out.println("ID EXISTE");
+                    verificador = true;
+                    break;
+                }
+                aux = aux.siguiente;
+            }while(aux != primero);
+            if(!verificador){
             ultimo.siguiente = nuevo;
             nuevo.siguiente = primero;
             nuevo.atras = ultimo;
             ultimo = nuevo;
             primero.atras = ultimo;
+            }
         }
     }
     
+    public void modificar(int id, String modificacion){
+        Nodo buscador;
+        buscador = primero;
+        if(primero != null){
+            do{
+                if(buscador.objeto.id == id){
+                    buscador.objeto.nombre = modificacion;
+                }
+                buscador = buscador.siguiente;
+            }while(buscador != primero);
+        }
+        System.out.println("-----");
+        mostrar();
+    }
+    
+    
+    public void eliminar(int id){
+       Nodo buscador;
+        buscador = primero;
+        if(primero != null){
+            do{
+                if(buscador.objeto.id == id){
+                    buscador.siguiente = buscador.atras;
+                    
+                }
+            }while(buscador != primero);
+        }
+        
+    }
+    
+    
+    
+    public void mostrar(){
+        Nodo aux = new Nodo(); 
+        aux = primero;
+        do{
+            System.out.println(aux.objeto.nombre);
+            aux = aux.siguiente;
+            tamaño++;
+        }while(aux != primero);
+    }
+    
+    
+    
+    
+    public int Tamaño(){
+        return tamaño;
+    }
     
 }
